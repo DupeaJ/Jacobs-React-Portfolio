1877af51a4808ffb99a68d472e2fef3e45fe064a
unknown <jacobdupea@gmail.com>
2023-11-14 15:24:34 -0800

    updated resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,9 +2,13 @@ import Header from '../components/Header';
 
 export default function Resume() {
 	return (
-		<section className="container ">
+		<section className="container resume-section" >
 			<div className="container">
 				<Header header={'Resume'} />
+				<a href="https://docs.google.com/document/d/19LccNFUAVtFbuGwa93WXRE-RdG2PMhsEDjlRWAJtkXY/edit?usp=sharing"
+					target="_blank"
+					rel="noreferrer"
+				>Click here to view resume</a>
 			</div>
 		</section>
 	);
