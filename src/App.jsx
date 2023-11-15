451dec8a1233fc04f6f37fea87e87b6074c60f18
unknown <jacobdupea@gmail.com>
2023-11-14 16:31:40 -0800

    added curser and on hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-import { Outlet } from 'react-router-dom';
-import Nav from './components/Navbar/Nav';
-import Footer from './components/footer/Footer';
-import './App.css';
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Navbar/Nav";
+import Footer from "./components/footer/Footer";
+import "./App.css";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
     return (
         <div id="app-container">
+            <AnimatedCursor
+                innerSize={8}
+                outerSize={8}
+                color="255, 0, 0"
+                outerAlpha={1.4}
+                innerScale={0.9}
+                outerScale={5}
+                trailingSpeed={15}
+                outerStyle={{
+                    border: "3px solid #fff",
+                }}
+                clickables={[
+                    "a",
+                    'input[type="text"]',
+                    'input[type="email"]',
+                    'input[type="number"]',
+                    'input[type="submit"]',
+                    'input[type="image"]',
+                    "label[for]",
+                    "select",
+                    "textarea",
+                    "button",
+                    ".link",
+                    "svg",
+                    {
+                        target: ".custom",
+                        options: {
+                            outerSize: 12,
+                            color: "255,0,0",
+                            outerAlpha: 0.4,
+                            innerScale: 0.7,
+                            outerScale: 5,
+                        },
+                    },
+                ]}
+            />
             <Nav />
             <main id="root">
                 <Outlet />
